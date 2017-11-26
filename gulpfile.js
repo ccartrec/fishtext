220e3284d9882291e6a38cc9214d7c1652a7e46b
Александр <cahei29@rambler.ru>
2017-11-26 22:41:05 +0300

    Добавлена gulp сборка проекта

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    del  = require('del');
+
+
+
+gulp.task('js', function () {
+    gulp.src('app/js/app.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('clean', function() {
+    return del.sync('build');
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/*')
+        .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('html', function() {
+    return gulp.src('app/index.html')
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['clean', 'html', 'sass', 'js', 'img'], function() {});
